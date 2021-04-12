d210c86096fc44348474212a99101cb41b8eb039
Santiago Lozano Ventura <santiago.ventura2012@gmail.com>
2021-04-12 11:51:56 -0600

    heroku add

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'static/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :api do
@@ -7,4 +8,5 @@ Rails.application.routes.draw do
     post "test_upload1", to: "uzers#test_upload1"
     get "uzers", to: "uzers#index"
   end
+  get '*other', to: 'static#index'
 end
